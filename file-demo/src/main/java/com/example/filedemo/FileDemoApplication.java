22ff4381053735e4448c7364e67859aa43b5f44e
venka <venka@10.1.10.230>
2018-05-29 13:07:35 -0400

    second commit

--- file-demo/src/main/java/com/example/filedemo/FileDemoApplication.java
+++ file-demo/src/main/java/com/example/filedemo/FileDemoApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class FileDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hai");
 		SpringApplication.run(FileDemoApplication.class, args);
 	}
 }
